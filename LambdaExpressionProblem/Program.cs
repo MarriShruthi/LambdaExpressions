72b15814ba68ba1d4afdc7c3574329662e061220
Shruthi <shruthimarri97@gmail.com>
2021-04-28 05:58:36 +0530

    [Shruthi] : Retriving age between range 13 to 18

--- LambdaExpressionProblem/Program.cs
+++ LambdaExpressionProblem/Program.cs
@@ -25,23 +25,22 @@ namespace LambdaExpressionProblem
         //UC1 person management
         public static void AddingPersonDetails(List<Person> personList) //creating method
         {
-            personList.Add(new Person() { SSN = 1, Age = 18,  Name = "Shruthi",  Address = "Hyderabad" });
-            personList.Add(new Person() { SSN = 2, Age = 28,  Name = "Swetha",   Address = "Mumbai" });
+            personList.Add(new Person() { SSN = 1, Age = 16,  Name = "Shruthi",  Address = "Hyderabad" });
+            personList.Add(new Person() { SSN = 2, Age = 18,  Name = "Swetha",   Address = "Mumbai" });
             personList.Add(new Person() { SSN = 3, Age = 30,  Name = "Rishika",  Address = "Bangalore" });
             personList.Add(new Person() { SSN = 5, Age = 48,  Name = "Nikhila",  Address = "pune" });
             personList.Add(new Person() { SSN = 6, Age = 58,  Name = "Preethi",  Address = "Chennai" });
             personList.Add(new Person() { SSN = 7, Age = 68,  Name = "Hima",     Address = "Hyderabad" });
             //IterateOverList(personList);
-            RetrieveTop2(personList);
+            Retrieve(personList);
         }
-        //UC2 Retrieve top two age less than 60
-        public static void RetrieveTop2(List<Person> list)
+        //UC3
+        public static void Retrieve(List<Person> list)
         {
-            var result = list.FindAll(x => x.Age < 60).OrderBy(x => x.Age).Take(2);
+            var result = list.FindAll(x => x.Age > 13 && x.Age < 18);
             foreach (Person person in result)
             {
-                Console.WriteLine("Age\t" + person.Age +"\t"+ "Name\t" + person.Name + "\t" + "Address\t" + person.Address);
-
+                Console.WriteLine("Age\t" + person.Age + "Name\t" + person.Name + "\t" + "Address\t" + person.Address);
             }
         }
     }
