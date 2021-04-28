33db595823c10766c918761f7c8927eeddd48eee
Shruthi <shruthimarri97@gmail.com>
2021-04-28 06:02:38 +0530

    [Shruthi] : Retriving Average Age

--- LambdaExpressionProblem/Program.cs
+++ LambdaExpressionProblem/Program.cs
@@ -25,22 +25,28 @@ namespace LambdaExpressionProblem
         //UC1 person management
         public static void AddingPersonDetails(List<Person> personList) //creating method
         {
-            personList.Add(new Person() { SSN = 1, Age = 16,  Name = "Shruthi",  Address = "Hyderabad" });
-            personList.Add(new Person() { SSN = 2, Age = 18,  Name = "Swetha",   Address = "Mumbai" });
-            personList.Add(new Person() { SSN = 3, Age = 30,  Name = "Rishika",  Address = "Bangalore" });
-            personList.Add(new Person() { SSN = 5, Age = 48,  Name = "Nikhila",  Address = "pune" });
-            personList.Add(new Person() { SSN = 6, Age = 58,  Name = "Preethi",  Address = "Chennai" });
-            personList.Add(new Person() { SSN = 7, Age = 68,  Name = "Hima",     Address = "Hyderabad" });
+            personList.Add(new Person() { SSN = 1, Age = 16, Name = "Shruthi", Address = "Hyderabad" });
+            personList.Add(new Person() { SSN = 2, Age = 18, Name = "Swetha", Address = "Mumbai" });
+            personList.Add(new Person() { SSN = 3, Age = 30, Name = "Rishika", Address = "Bangalore" });
+            personList.Add(new Person() { SSN = 5, Age = 48, Name = "Nikhila", Address = "pune" });
+            personList.Add(new Person() { SSN = 6, Age = 58, Name = "Preethi", Address = "Chennai" });
+            personList.Add(new Person() { SSN = 7, Age = 68, Name = "Hima", Address = "Hyderabad" });
             //IterateOverList(personList);
             Retrieve(personList);
         }
         //UC3
         public static void Retrieve(List<Person> list)
         {
-            var result = list.FindAll(x => x.Age > 13 && x.Age < 18);
-            foreach (Person person in result)
+            try
             {
-                Console.WriteLine("Age\t" + person.Age + "Name\t" + person.Name + "\t" + "Address\t" + person.Address);
+                var result = list.Average(x => x.Age);
+
+                Console.WriteLine("Average age among all persons\t" + result);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
